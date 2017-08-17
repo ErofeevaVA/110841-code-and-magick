5dcf694d8fd107eb17afa132889910ad79c5e628
ErofeevaVA <erof.vika@gmail.com>
2017-08-17 16:16:54 +0300

    Исправление ошибок: добавление функций

--- js/stat.js
+++ js/stat.js
@@ -3,11 +3,13 @@
 window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'white';
   ctx.strokeRect(100, 10, 420, 270);
-  //ctx.shadowOffsetX = 10; почему-то тень появляется у всех элементов? а как сделать тень только у прямоугольника я не додумалась
-  //ctx.shadowOffsetY = 10;
-  //ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
+  ctx.shadowOffsetX = 10;
+  ctx.shadowOffsetY = 10;
+  ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(100, 10, 420, 270);
 
+  ctx.shadowOffsetX = 0;
+  ctx.shadowOffsetY = 0;
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
 
@@ -17,7 +19,7 @@ window.renderStatistics = function (ctx, names, times) {
   var max = -1;
   var maxIndex = -1;
 
-  for (var i = 0 ; i < times.length; i++) {
+  for (var i = 0; i < times.length; i++) {
    var time = times[i];
     if (time > max) {
       max = time;
@@ -35,21 +37,27 @@ window.renderStatistics = function (ctx, names, times) {
 
   ctx.textBaseline = 'bottom';
 
-  var getRandomArbitary = function(min, max) {
-        min = 0.1;
-        max = 1;
-        return Math.random() * (max - min) + min;
-      }
+  var getRandomArbitary = function (min, max) {
+    min = 0.1;
+    max = 1;
+    return Math.random() * (max - min) + min;
+  }
 
-  for(var i = 0; i < times.length; i++) {
+  for (var i = 0; i < times.length; i++) {
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
         ctx.fillStyle = 'rgba(0, 0, 255, ' + getRandomArbitary() + ')';
       }
-    ctx.fillRect(initialX + (indent * i + barWidth * i), initialY - times[i] * step, barWidth, times[i] * step);
+    var getInitialX = function() {
+      return initialX + (indent * i + barWidth * i);
+    }
+    var getInitialY = function() {
+      return initialY - times[i] * step;
+    }
+    ctx.fillRect(getInitialX(), getInitialY(), barWidth, times[i] * step);
     ctx.fillStyle = 'black';
-    ctx.fillText(Math.round(times[i]), initialX + (indent * i + barWidth * i), initialY - times[i] * step - 10);
-    ctx.fillText(names[i], initialX + (indent * i + barWidth * i), initialY + 20);
+    ctx.fillText(Math.round(times[i]), getInitialX(), getInitialY() - 10);
+    ctx.fillText(names[i], getInitialX(), initialY + 20);
   }
 };
